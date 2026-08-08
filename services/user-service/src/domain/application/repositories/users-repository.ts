0bb3cfec6b77d1f4f520df793d424945030fef46
Artur Santos <arturcastrodossantos.com@gmail.com>
2026-08-08 14:18:19 -0300

    feat(user-service): implementado configs do prisma e criando schema de user

--- services/user-service/src/domain/application/repositories/users-repository.ts
+++ services/user-service/src/domain/application/repositories/users-repository.ts
@@ -1,9 +1,11 @@
 import { User } from "../../enterprise/entities/user";
 
-export abstract class UsersRepository {
+export abstract class UserRepository {
   abstract create(user: User): Promise<void>
   abstract update(user: User): Promise<void>
   abstract delete(user: User): Promise<void>
   abstract findByEmail(email: string): Promise<User | null>
   abstract findById(id: string): Promise<User | null>
 }
+
+export { UserRepository as UsersRepository }
