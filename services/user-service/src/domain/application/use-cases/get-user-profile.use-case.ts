fac4a26a7ca69ec6ddc59198f25d1c10e866f074
Artur Santos <arturcastrodossantos.com@gmail.com>
2026-08-08 12:46:03 -0300

    feat(user-service): adicionar busca de perfil

---
+++ services/user-service/src/domain/application/use-cases/get-user-profile.use-case.ts
@@ -0,0 +1,37 @@
+import { UserNotFoundError } from '../../../core/errors/user-not-found.error'
+import { UserType } from '../../enterprise/entities/user'
+import { UsersRepository } from '../repositories/users-repository'
+
+export interface GetUserProfileUseCaseRequest {
+  userId: string
+}
+
+export interface UserProfile {
+  id: string
+  name: string
+  email: string
+  userType: keyof typeof UserType
+  createdAt: Date
+  updatedAt?: Date | null
+}
+
+export class GetUserProfileUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({ userId }: GetUserProfileUseCaseRequest): Promise<UserProfile | UserNotFoundError> {
+    const user = await this.usersRepository.findById(userId)
+
+    if (!user) {
+      return new UserNotFoundError()
+    }
+
+    return {
+      id: user.id.toString(),
+      name: user.name,
+      email: user.email,
+      userType: UserType[user.userType],
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    }
+  }
+}
