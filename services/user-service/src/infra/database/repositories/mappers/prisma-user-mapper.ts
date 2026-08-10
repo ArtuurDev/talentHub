83f6e058de82e5b5952fe3df2289bcdb6500120c
Artur Santos <arturcastrodossantos.com@gmail.com>
2026-08-10 15:39:55 -0300

    feat(user-service): implementar sessões com refresh token

---
+++ services/user-service/src/infra/database/repositories/mappers/prisma-user-mapper.ts
@@ -0,0 +1,37 @@
+import { User as PrismaUser, UserType as PrismaUserType } from '@prisma/client'
+import { UniqueEntityId } from '../../../../core/entities/unique-entity-id'
+import { User, UserType } from '../../../../domain/enterprise/entities/user'
+
+export class PrismaUserMapper {
+  static toPrisma(user: User) {
+    return {
+      id: user.id.toString(),
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      userType: UserType[user.userType] as PrismaUserType,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    }
+  }
+
+  static toDomain(user: PrismaUser): User {
+    const domainUser = User.create(
+      {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        userType: UserType[user.userType],
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      },
+      new UniqueEntityId(user.id),
+    )
+
+    if (!(domainUser instanceof User)) {
+      throw new Error('Usuário persistido possui e-mail inválido')
+    }
+
+    return domainUser
+  }
+}
