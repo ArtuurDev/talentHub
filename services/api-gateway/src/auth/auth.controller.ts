a1be80b8efe7e7695bb5035a2f08c5ecc137f539
Artur Santos <arturcastrodossantos.com@gmail.com>
2026-08-12 16:42:58 -0300

    feat(api-gateway): adicionar refrehToken nos cookies

--- services/api-gateway/src/auth/auth.controller.ts
+++ services/api-gateway/src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Body, Controller, Headers, Post, Req } from "@nestjs/common";
+import { Body, Controller, Headers, Post, Req, Res } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import z from "zod/v3";
 import { ZodValidationPipe } from "../common/pipes/zod-validation.pipe";
 import type { IncomingHttpHeaders } from "node:http";
 import { Public } from '../common/decorators/public';
+import type { Response } from "express";
+import { ConfigService } from "@nestjs/config";
+import { Env } from "../env";
 
 export const loginSchema = z.object({
   password: z.string(),
@@ -22,20 +25,31 @@ type LoginDTO = z.infer<typeof loginSchema>
 
 @Controller('users')
 export class AuthController{
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private configService: ConfigService<Env>
+  ) {}
 
   @Post('login')
   @Public()
   async login(@Body(new ZodValidationPipe(loginSchema)) {
     email,
     password
-  }: LoginDTO, @Headers() headers: IncomingHttpHeaders ) {
-    return this.authService.login({
+  }: LoginDTO, @Headers() headers: IncomingHttpHeaders, @Res() response: Response ) {
+    const result = await this.authService.login({
       email,
       method: 'POST',
       password,
       path: 'users/login',
       headers: headers,
+    })
+
+    const sevenDaysInMilisseconds = 604800000
+    response.cookie('refreshToken', result?.data.refreshToken, {
+      httpOnly: true,
+      secure: this.configService.get("NODE_ENV") === 'production',
+      sameSite: 'strict',
+      maxAge: sevenDaysInMilisseconds
     })
   }
 
